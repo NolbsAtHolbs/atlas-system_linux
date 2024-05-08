1115330da2229a1717c334f64ac116e1c6f02e29
NolbsAtHolbs <you@example.com>
2024-05-08 07:17:10 -0500

    compliance fix, need automated

--- getline/laps.c
+++ getline/laps.c
@@ -1,53 +1,50 @@
 #include "laps.h"
 
 /**
- * free_cars - recursively frees cars with false head that points to list
- * @car: pointer to cars struct
-*/
-
-static car_sdt dummyHead = {.nextCar = NULL};
-
-static void free_cars(car_sdt *car) {
-    if (car == NULL) return;
-    free_cars(car->nextCar);
-    free(car);
-}
-
-/**
  * race_state - counts laps driven by the cars in the race
  * @id: array of ints of ids of cars
  * @size: size of id array
 */
 
-void race_state(int *id, size_t size) {
-    car_sdt *previousCar = &dummyHead;
-    car_sdt *currentCar = dummyHead.nextCar;
+void race_state(int *id, size_t size)
+{
+	static car_sdt *cars;
+	car_sdt *nextCar, *previousCar, *Car;
 
-    if (size == 0) {
-        free_cars(dummyHead.nextCar);
-        dummyHead.nextCar = NULL;
-        return;
-    }
+	if (size == 0)
+	{
+		for (; cars; cars = nextCar)
+			nextCar = cars->nextCar, free(cars);
+		return;
+	}
 
-    for (; size--; id++) {
-        for (; currentCar && *id > currentCar->id; previousCar = currentCar, currentCar = currentCar->nextCar);
+	for (; size--; id++)
+	{
+		for (previousCar = NULL, nextCar = cars; nextCar; previousCar = nextCar, nextCar = nextCar->nextCar)
+			if (*id <= nextCar->id)
+				break;
 
-        if (currentCar && *id == currentCar->id)
+		if (nextCar && *id == nextCar->id)
 		{
-            currentCar->laps++;
-        } else {
-            printf("Car %d joined the race\n", *id);
-            car_sdt *newCar = malloc(sizeof(car_sdt));
-            if (!newCar) return;
-            newCar->id = *id;
-            newCar->laps = 1;
-            newCar->nextCar = currentCar;
-            previousCar->nextCar = newCar;
-        }
-    }
+			nextCar->laps = nextCar->laps + 1;
+		}
+		else
+		{
+			printf("Car %d joined the race\n", *id);
+			Car = malloc(sizeof(car_sdt));
+			if (Car == NULL)
+				return;
+			Car->id = *id;
+			Car->laps = 0;
+			Car->nextCar = nextCar;
+			if (previousCar)
+				previousCar->nextCar = Car;
+			else
+				cars = Car;
+		}
+	}
 
-    printf("Race state:\n");
-    for (currentCar = dummyHead.nextCar; currentCar; currentCar = currentCar->nextCar) {
-        printf("Car %d [%d laps]\n", currentCar->id, currentCar->laps);
-    }
+	printf("Race state:\n");
+	for (nextCar = cars; nextCar; nextCar = nextCar->nextCar)
+		printf("Car %d [%d laps]\n", nextCar->id, nextCar->laps);
 }
