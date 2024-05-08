a00da8d6e14eb20176dea7e6bb30c20a16534d43
NolbsAtHolbs <you@example.com>
2024-05-08 10:15:04 -0500

    basis

---
+++ getline/_getline.c
@@ -0,0 +1,12 @@
+#include "_getline.h"
+
+/**
+ * _getline - reads a line from a file descriptor
+ * @fd: a file descriptor
+ * Return: null terminated string or NULL if error || no line
+*/
+
+char *_getline(const int fd)
+{
+	
+}
---
+++ getline/_getline.h
@@ -0,0 +1,14 @@
+#ifndef _GETLINE_H
+#define _GETLINE_H
+
+#define READ_SIZE 1024
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+char *_getline(const int fd);
+
+#endif
