674e495bc678d673b804fd9b52aca5af8b583b21
NolbsAtHolbs <you@example.com>
2024-05-08 13:13:27 -0500

    revert

--- getline/_getline.c
+++ getline/_getline.c
@@ -1,11 +1,9 @@
 #include "_getline.h"
-
 /**
  * _getline - reads a line from a file descriptor
  * @fd: a file descriptor
  * Return: null terminated string, or NULL if error or no line
 */
-
 char *_getline(const int fd)
 {
 	static char buffer[READ_SIZE + 1]; /* account for null terminator */
@@ -14,7 +12,6 @@ char *_getline(const int fd)
 	char *line = NULL;
 	int i = position;
 	int j;
-
 	while (1)
 	{
 		if (position >= bytes_read)
@@ -22,23 +19,7 @@ char *_getline(const int fd)
 			position = 0;
 			bytes_read = read(fd, buffer, READ_SIZE);
 			if (bytes_read == 0)
-			{
-				if (i < position)
-				{
-					line = malloc((position - i + 1) * sizeof(char));
-					if (!line)
-						return (NULL);
-					for (j = i; j < position; j++)
-						line[j - i] = buffer[j];
-					line[position - i] = '\0';
-					position++;
-					return (line);
-				}
-                else
-                {
-                    return (NULL);
-                }
-			}
+				return (NULL);
 			if (bytes_read == -1) /* account for _getline read error */
 				return (NULL);
 			buffer[bytes_read] = '\0'; /* mark end of string */
