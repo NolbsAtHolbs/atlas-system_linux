d346f6e5e16933b2b350e446a69e71ee7fb8c0ba
NolbsAtHolbs <you@example.com>
2024-05-08 12:38:44 -0500

    custom getline function

--- getline/_getline.c
+++ getline/_getline.c
@@ -3,10 +3,42 @@
 /**
  * _getline - reads a line from a file descriptor
  * @fd: a file descriptor
- * Return: null terminated string or NULL if error || no line
+ * Return: null terminated string, or NULL if error or no line
 */
 
 char *_getline(const int fd)
 {
-	
+	static char buffer[READ_SIZE + 1]; /* account for null terminator */
+	static int position = 0;
+	static int bytes_read = 0;
+	char *line = NULL;
+	int i = position;
+	int j;
+
+	while (1)
+	{
+		if (position >= bytes_read)
+		{
+			position = 0;
+			bytes_read = read(fd, buffer, READ_SIZE);
+			if (bytes_read == 0)
+				return NULL;
+			if (bytes_read == -1) /* account for _getline read error */
+				return NULL;
+			buffer[bytes_read] = '\0'; /* mark end of string */
+		}
+		if (buffer[position] == '\n')
+		{
+			line = malloc((position - i + 1) * sizeof(char));
+			if (!line)
+				return NULL;
+			for (j = i; j < position; j++)
+				line[j - i] = buffer[j]; /* copy buffer array to line array */
+			line[position - i] = '\0';
+			position++;
+			return line;
+		}
+		position++;
+	}
+	return NULL; 
 }
