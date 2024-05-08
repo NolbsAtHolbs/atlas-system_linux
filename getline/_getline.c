044b02d4ca5b95984788e3eccd66157fa2c967ae
NolbsAtHolbs <you@example.com>
2024-05-08 12:46:11 -0500

    compliance

--- getline/_getline.c
+++ getline/_getline.c
@@ -9,8 +9,8 @@
 char *_getline(const int fd)
 {
 	static char buffer[READ_SIZE + 1]; /* account for null terminator */
-	static int position = 0;
-	static int bytes_read = 0;
+	static int position;
+	static int bytes_read;
 	char *line = NULL;
 	int i = position;
 	int j;
@@ -22,23 +22,23 @@ char *_getline(const int fd)
 			position = 0;
 			bytes_read = read(fd, buffer, READ_SIZE);
 			if (bytes_read == 0)
-				return NULL;
+				return (NULL);
 			if (bytes_read == -1) /* account for _getline read error */
-				return NULL;
+				return (NULL);
 			buffer[bytes_read] = '\0'; /* mark end of string */
 		}
 		if (buffer[position] == '\n')
 		{
 			line = malloc((position - i + 1) * sizeof(char));
 			if (!line)
-				return NULL;
+				return (NULL);
 			for (j = i; j < position; j++)
 				line[j - i] = buffer[j]; /* copy buffer array to line array */
 			line[position - i] = '\0';
 			position++;
-			return line;
+			return (line);
 		}
 		position++;
 	}
-	return NULL; 
+	return (NULL);
 }
