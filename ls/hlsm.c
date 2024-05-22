5c4ac4ec7965d422d20cfc23089e3fd0f4ed5639
NolbsAtHolbs <you@example.com>
2024-05-22 18:13:29 -0500

    handle -1 option

--- ls/hlsm.c
+++ ls/hlsm.c
@@ -1,7 +1,8 @@
 #include "hls.h"
 
-void list_directory(char *path, char *program_name);
+void list_directory(char *path, char *program_name, int single_column);
 char **read_entries(DIR *dir, int *count, char *program_name);
+int is_option(char *arg, const char *option);
 
 /**
 * main - main program
@@ -14,10 +15,18 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int first = 1;
+	int single_column = 0;
+
+	if (argc > 1 && is_option(argv[1], "-1"))
+	{
+		single_column = 1;
+		argv++;
+		argc--;
+	}
 
 	if (argc == 1)
 	{
-		list_directory(".", argv[0]);
+		list_directory(".", argv[0], single_column);
 	}
 	else
 	{
@@ -44,9 +53,33 @@ int main(int argc, char *argv[])
 				first = 0;
 			}
 
-			list_directory(argv[i], argv[0]); /* curious */
+			list_directory(argv[i], argv[0], single_column);
 		}
 	}
+	return (0);
+}
+
+/**
+ * is_option - Checks if an argument is an option
+ * @arg: The argument to check
+ * @option: The option to check for
+ *
+ * Return: 1 if the argument matches the option, 0 otherwise
+ */
+int is_option(char *arg, const char *option)
+{
+	int i = 0;
+
+	while (arg[i] != '\0' && option[i] != '\0' && arg[i] == option[i])
+	{
+		i++;
+	}
+
+	if (arg[i] == '\0' && option[i] == '\0')
+	{
+		return (1);
+	}
+
 	return (0);
 }
 
@@ -54,9 +87,9 @@ int main(int argc, char *argv[])
 * list_directory - Lists the contents of a directory
 * @path: Path to the directory
 * @program_name: Name of the program (argv[0])
-* @show_path: Flag indicating when path is shown
+* @single_column: Flag for single column output
 */
-void list_directory(char *path, char *program_name)
+void list_directory(char *path, char *program_name, int single_column)
 {
 	DIR *dir;
 	char **entries;
@@ -79,10 +112,21 @@ void list_directory(char *path, char *program_name)
 
 	sort_entries(entries, count);
 
-	for (count = 0; entries[count] != NULL; count++)
+	if (single_column)
 	{
-		printf("%s\n", entries[count]);
-		free(entries[count]);
+		for (count = 0; entries[count] != NULL; count++)
+		{
+			printf("%s\n", entries[count]);
+			free(entries[count]);
+		}
+	}
+	else
+	{
+		for (count = 0; entries[count] != NULL; count++)
+		{
+			printf("%s\n", entries[count]);
+			free(entries[count]);
+		}
 	}
 	free(entries);
 }
