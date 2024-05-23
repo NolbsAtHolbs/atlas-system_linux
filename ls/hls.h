40d15e902cb7de42d6388f8e85dd2f0f52214bd9
NolbsAtHolbs <you@example.com>
2024-05-22 21:47:26 -0500

    handle case insensitive folder names

--- ls/hls.h
+++ ls/hls.h
@@ -12,13 +12,15 @@ void *xmalloc(size_t size, char *program_name);
 void *xrealloc(void *ptr, size_t size, char *program_name);
 void sort_entries(char **entries, int count);
 int compare_strings(const char *a, const char *b);
-int is_option(char *arg, const char *option);
+int is_option_multiple(char *arg, const char *option);
 void list_directory(char *path, char *program_name, int single_column);
 char **read_entries(DIR *dir, int *count, char *program_name);
 void process_arguments(int argc, char *argv[], int *single_column, int *start);
 int count_files_and_folders(int argc, char *argv[], int start);
 void sort_paths(int argc, char *argv[], int start);
 int compare_paths(const char *a, const char *b);
-int is_option_multiple(char *arg, const char *option);
+void list_files(int argc, char *argv[], int start);
+void list_directories(int argc, char *argv[], int start,
+						int single_column, char *program_name);
 
 #endif
--- ls/hlsm.c
+++ ls/hlsm.c
@@ -9,37 +9,22 @@
 */
 int main(int argc, char *argv[])
 {
-	int i;
 	int single_column = 0;
 	int start = 1;
 	int file_count = 0;
-	DIR *dir;
 
 	process_arguments(argc, argv, &single_column, &start);
 	file_count = count_files_and_folders(argc, argv, start);
+
 	if (file_count == 0)
+	{
 		list_directory(".", argv[0], single_column);
+	}
 	else
 	{
 		sort_paths(argc, argv, start);
-		for (i = start; i < argc; i++)
-		{
-			if (is_option_multiple(argv[i], "-1"))
-				continue;
-			dir = opendir(argv[i]);
-			if (dir == NULL && errno != ENOTDIR)
-			{
-				handle_errors(argv[0], argv[i]);
-				continue;
-			}
-			if (dir != NULL)
-				closedir(dir);
-			if (file_count > 1 && dir != NULL)
-				printf("%s:\n", argv[i]);
-			list_directory(argv[i], argv[0], single_column);
-			if (i < argc - 1 && !is_option_multiple(argv[i + 1], "-1"))
-				printf("\n");
-		}
+		list_files(argc, argv, start);
+		list_directories(argc, argv, start, single_column, argv[0]);
 	}
 	return (0);
 }
@@ -67,29 +52,6 @@ void process_arguments(int argc, char *argv[], int *single_column, int *start)
 			break;
 		}
 	}
-}
-
-/**
-* is_option - Checks if an argument is an option
-* @arg: The argument to check
-* @option: The option to check for
-*
-* Return: 1 if the argument matches the option, 0 otherwise
-*/
-int is_option(char *arg, const char *option)
-{
-	int i = 0;
-
-	while (arg[i] != '\0' && option[i] != '\0' && arg[i] == option[i])
-	{
-		i++;
-	}
-
-	if (arg[i] == '\0' && option[i] == '\0')
-	{
-		return (1);
-	}
-	return (0);
 }
 
 /**
