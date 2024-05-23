a5d35cd997b65b84413b12ad3f2659822d602611
NolbsAtHolbs <you@example.com>
2024-05-22 21:58:56 -0500

    test

--- ls/hlshe.c
+++ ls/hlshe.c
@@ -1,5 +1,4 @@
 #include "hls.h"
-
 /**
  * count_files_and_folders - Counts the number of file or folder arguments
  * @argc: Number of arguments
@@ -12,18 +11,16 @@ int count_files_and_folders(int argc, char *argv[], int start)
 {
     int i;
     int count;
-
     count = 0;
     for (i = start; i < argc; i++)
     {
-        if (!is_option_multiple(argv[i], "-1"))
+        if (!is_option(argv[i], "-1"))
         {
             count++;
         }
     }
     return (count);
 }
-
 /**
  * sort_paths - Sorts the paths for directory listing priority
  * @argc: Number of arguments
@@ -35,7 +32,6 @@ void sort_paths(int argc, char *argv[], int start)
     int i;
 	int j;
 	char *temp;
-
     for (i = start; i < argc - 1; i++)
     {
         for (j = i + 1; j < argc; j++)
@@ -53,7 +49,6 @@ void sort_paths(int argc, char *argv[], int start)
         }
     }
 }
-
 /**
  * compare_paths - Simple comparison of two paths to ensure parent paths come first
  * @a: First path
@@ -61,34 +56,28 @@ void sort_paths(int argc, char *argv[], int start)
  *
  * Return: Negative if a < b, zero if a == b, positive if a > b
  */
-int compare_paths(const char *a, const char *b)
+int compare_paths(char *a, char *b)
 {
-	char lower_a;
-	char lower_b;
-
-	while (*a && *b)
-	{
-		lower_a = (*a >= 'A' && *a <= 'Z') ? *a + 32 : *a;
-		lower_b = (*b >= 'A' && *b <= 'Z') ? *b + 32 : *b;
-
-		if (lower_a < lower_b)
-		{
-			return (-1);
-		}
-		if (lower_a > lower_b)
-		{
-			return (1);
-		}
-		a++;
-		b++;
-	}
-	if (*a == '\0' && *b == '\0')
-	{
-		return (0);
-	}
-	if (*a == '\0')
-	{
-		return (-1);
-	}
-	return (1);
+    while (*a && *b)
+    {
+        if (*a < *b)
+        {
+            return (-1);
+        }
+        if (*a > *b)
+        {
+            return (1);
+        }
+        a++;
+        b++;
+    }
+    if (*a == '\0' && *b == '\0')
+    {
+        return (0);
+    }
+    if (*a == '\0')
+    {
+        return (-1);
+    }
+    return (1);
 }
--- ls/hlsm.c
+++ ls/hlsm.c
@@ -1,5 +1,4 @@
 #include "hls.h"
-
 /**
 * main - main program
 * @argc: Number of arguments
@@ -9,26 +8,39 @@
 */
 int main(int argc, char *argv[])
 {
+	int i;
 	int single_column = 0;
 	int start = 1;
 	int file_count = 0;
-
+	DIR *dir;
 	process_arguments(argc, argv, &single_column, &start);
 	file_count = count_files_and_folders(argc, argv, start);
-
 	if (file_count == 0)
-	{
 		list_directory(".", argv[0], single_column);
-	}
 	else
 	{
 		sort_paths(argc, argv, start);
-		list_files(argc, argv, start);
-		list_directories(argc, argv, start, single_column, argv[0]);
+		for (i = start; i < argc; i++)
+		{
+			if (is_option_multiple(argv[i], "-1"))
+				continue;
+			dir = opendir(argv[i]);
+			if (dir == NULL && errno != ENOTDIR)
+			{
+				handle_errors(argv[0], argv[i]);
+				continue;
+			}
+			if (dir != NULL)
+				closedir(dir);
+			if (file_count > 1 && dir != NULL)
+				printf("%s:\n", argv[i]);
+			list_directory(argv[i], argv[0], single_column);
+			if (i < argc - 1 && !is_option_multiple(argv[i + 1], "-1"))
+				printf("\n");
+		}
 	}
 	return (0);
 }
-
 /**
 * process_arguments - Processes command-line arguments
 * @argc: Number of arguments
@@ -42,7 +54,7 @@ void process_arguments(int argc, char *argv[], int *single_column, int *start)
 
 	for (i = 1; i < argc; i++)
 	{
-		if (is_option_multiple(argv[i], "-1"))
+		if (is_option(argv[i], "-1"))
 		{
 			*single_column = 1;
 			(*start)++;
@@ -53,7 +65,26 @@ void process_arguments(int argc, char *argv[], int *single_column, int *start)
 		}
 	}
 }
-
+/**
+* is_option - Checks if an argument is an option
+* @arg: The argument to check
+* @option: The option to check for
+*
+* Return: 1 if the argument matches the option, 0 otherwise
+*/
+int is_option(char *arg, const char *option)
+{
+	int i = 0;
+	while (arg[i] != '\0' && option[i] != '\0' && arg[i] == option[i])
+	{
+		i++;
+	}
+	if (arg[i] == '\0' && option[i] == '\0')
+	{
+		return (1);
+	}
+	return (0);
+}
 /**
 * is_option_multiple - Checks if argument is valid -1 option
 * @arg: The argument to check
@@ -64,16 +95,13 @@ void process_arguments(int argc, char *argv[], int *single_column, int *start)
 int is_option_multiple(char *arg, const char *option)
 {
 	int i = 0;
-
 	if (arg[i] != '-' || option[i] != '-')
 		return (0);
-
 	i++;
 	while (arg[i] != '\0' && option[1] != '\0' && arg[i] == option[1])
 	{
 		i++;
 	}
-
 	if (arg[i] == '\0')
 	{
 		return (1);
