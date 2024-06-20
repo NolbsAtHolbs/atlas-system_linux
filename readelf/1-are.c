7def849cb99a591b8fd27e4d3a3cc9ae8507e718
NolbsAtHolbs <you@example.com>
2024-06-20 01:51:49 -0500

    improve sect header display

--- readelf/1-are.c
+++ readelf/1-are.c
@@ -14,59 +14,59 @@ prog_dt prog;
  */
 int main(int argc, char **argv)
 {
-	elf_dt elfen;
-	struct stat st;
-	char *map;
+    elf_dt elfen;
+    struct stat st;
+    char *map;
 
-	memset(&elfen, 0, sizeof(elfen));
+    memset(&elfen, 0, sizeof(elfen));
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s ELF-file\n", argv[0]);
-		return (1);
-	}
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s ELF-file\n", argv[0]);
+        return (1);
+    }
 
-	prog.name = argv[0];
-	prog.file = argv[1];
-	prog.fd = open(argv[1], O_RDONLY);
+    prog.name = argv[0];
+    prog.file = argv[1];
+    prog.fd = open(argv[1], O_RDONLY);
 
-	if (prog.fd == -1)
-	{
-		error_manager(1, 1);
-	}
+    if (prog.fd == -1)
+    {
+        error_manager(1, 1);
+    }
 
-	if (fstat(prog.fd, &st) != 0)
-	{
-		error_manager(1, 1);
-	}
+    if (fstat(prog.fd, &st) != 0)
+    {
+        error_manager(1, 1);
+    }
 
-	map = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, prog.fd, 0);
+    map = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, prog.fd, 0);
 
-	if (map == MAP_FAILED)
-	{
-		error_manager(1, 1);
-	}
+    if (map == MAP_FAILED)
+    {
+        error_manager(1, 1);
+    }
 
-	if (!is_elf(map))
-	{
-		error_manager(2, 1);
-	}
+    if (!is_elf(map))
+    {
+        error_manager(2, 1);
+    }
 
-	memcpy(&elfen.fh64, map, sizeof(elfen.fh64));
+    memcpy(&elfen.fh64, map, sizeof(elfen.fh64));
 
-	set_class(elfen.fh64.e_ident[EI_CLASS]);
+    set_class(elfen.fh64.e_ident[EI_CLASS]);
 
-	if (prog.fclass == ELFCLASS32)
-	{
-		memcpy(&elfen.fh32, map, sizeof(elfen.fh32));
-	}
+    if (prog.fclass == ELFCLASS32)
+    {
+        memcpy(&elfen.fh32, map, sizeof(elfen.fh32));
+    }
 
-	set_endianness(elfen.fh64.e_ident[EI_DATA]);
-	process_section_headers(&elfen, map);
+    set_endianness(elfen.fh64.e_ident[EI_DATA]);
+    process_section_headers(&elfen, map);
 
-	close(prog.fd);
-	munmap(map, st.st_size);
-	return (0);
+    close(prog.fd);
+    munmap(map, st.st_size);
+    return (0);
 }
 
 /**
@@ -75,14 +75,14 @@ int main(int argc, char **argv)
  */
 static void set_class(unsigned char clas)
 {
-	if (clas == ELFCLASS32)
-	{
-		prog.fclass = ELFCLASS32;
-	}
-	else if (clas == ELFCLASS64)
-	{
-		prog.fclass = ELFCLASS64;
-	}
+    if (clas == ELFCLASS32)
+    {
+        prog.fclass = ELFCLASS32;
+    }
+    else if (clas == ELFCLASS64)
+    {
+        prog.fclass = ELFCLASS64;
+    }
 }
 
 /**
@@ -91,14 +91,14 @@ static void set_class(unsigned char clas)
  */
 static void set_endianness(unsigned char dat)
 {
-	if (dat == ELFDATA2LSB)
-	{
-		prog.endianness = 1;
-	}
-	else if (dat == ELFDATA2MSB)
-	{
-		prog.endianness = 2;
-	}
+    if (dat == ELFDATA2LSB)
+    {
+        prog.endianness = 1;
+    }
+    else if (dat == ELFDATA2MSB)
+    {
+        prog.endianness = 2;
+    }
 }
 
 /**
@@ -108,30 +108,30 @@ static void set_endianness(unsigned char dat)
  */
 static void process_section_headers(elf_dt *elfen, char *map)
 {
-	if (prog.fclass == ELFCLASS32)
-	{
-		elfen->sh32 = (Elf32_Shdr *)(map + elfen->fh32.e_shoff);
-		if (elfen->fh32.e_shstrndx < elfen->fh32.e_shnum)
-		{
-			print_section_headers_32(elfen->sh32, map + elfen->sh32[elfen->fh32.e_shstrndx].sh_offset, elfen->fh32.e_shnum);
-		}
-		else
-		{
-			fprintf(stderr, "Invalid section header string table index\n");
-			error_manager(2, 1);
-		}
-	}
-	else if (prog.fclass == ELFCLASS64)
-	{
-		elfen->sh64 = (Elf64_Shdr *)(map + elfen->fh64.e_shoff);
-		if (elfen->fh64.e_shstrndx < elfen->fh64.e_shnum)
-		{
-			print_section_headers_64(elfen->sh64, map + elfen->sh64[elfen->fh64.e_shstrndx].sh_offset, elfen->fh64.e_shnum);
-		}
-		else
-		{
-			fprintf(stderr, "Invalid section header string table index\n");
-			error_manager(2, 1);
-		}
-	}
+    if (prog.fclass == ELFCLASS32)
+    {
+        elfen->sh32 = (Elf32_Shdr *)(map + elfen->fh32.e_shoff);
+        if (elfen->fh32.e_shstrndx < elfen->fh32.e_shnum)
+        {
+            print_section_headers_32(elfen->sh32, map + elfen->sh32[elfen->fh32.e_shstrndx].sh_offset, elfen->fh32.e_shnum);
+        }
+        else
+        {
+            fprintf(stderr, "Invalid section header string table index\n");
+            error_manager(2, 1);
+        }
+    }
+    else if (prog.fclass == ELFCLASS64)
+    {
+        elfen->sh64 = (Elf64_Shdr *)(map + elfen->fh64.e_shoff);
+        if (elfen->fh64.e_shstrndx < elfen->fh64.e_shnum)
+        {
+            print_section_headers_64(elfen->sh64, map + elfen->sh64[elfen->fh64.e_shstrndx].sh_offset, elfen->fh64.e_shnum);
+        }
+        else
+        {
+            fprintf(stderr, "Invalid section header string table index\n");
+            error_manager(2, 1);
+        }
+    }
 }
--- readelf/1-shinfo.c
+++ readelf/1-shinfo.c
@@ -1,5 +1,8 @@
 #include "hreadelf.h"
 
+static const char *get_section_type(uint32_t sh_type);
+static const char *get_section_flags(uint64_t sh_flags);
+
 /**
  * print_section_headers_32 - prints section headers for 32-bit ELF file
  * @shdr: section header array for 32-bit ELF file
@@ -8,22 +11,28 @@
  */
 void print_section_headers_32(const Elf32_Shdr *shdr, const char *shstr, int num)
 {
-	int i;
+    int i;
 
-	printf("There are %d section headers, starting at offset 0x%x:\n\n",
-		num, shdr->sh_offset);
+    printf("There are %d section headers, starting at offset 0x%x:\n\n",
+           num, shdr->sh_offset);
 
-	printf("Section Headers:\n");
-	printf("  [Nr] Name              Type            Address          Off    Size   ES Flg Lk Inf Al\n");
+    printf("Section Headers:\n");
+    printf("  [Nr] Name              Type            Addr     Off    Size   ES Flg Lk Inf Al\n");
 
-	for (i = 0; i < num; i++)
-	{
-		printf("  [%2d] %-17s %-15s %08x %06x %06x %02x %3s %2d %3d %2d\n",
-			i, shstr + shdr[i].sh_name, "Section Type",
-			shdr[i].sh_addr, shdr[i].sh_offset, shdr[i].sh_size,
-			shdr[i].sh_entsize, "Flg", shdr[i].sh_link,
-			shdr[i].sh_info, shdr[i].sh_addralign);
-	}
+    for (i = 0; i < num; i++)
+    {
+        printf("  [%2d] %-17s %-15s %08x %06x %06x %02x %3s %2d %3d %2d\n",
+               i, shstr + shdr[i].sh_name, get_section_type(shdr[i].sh_type),
+               shdr[i].sh_addr, shdr[i].sh_offset, shdr[i].sh_size,
+               shdr[i].sh_entsize, get_section_flags(shdr[i].sh_flags),
+               shdr[i].sh_link, shdr[i].sh_info, shdr[i].sh_addralign);
+    }
+
+    printf("Key to Flags:\n");
+    printf("  W (write), A (alloc), X (execute), M (merge), S (strings), I (info),\n");
+    printf("  L (link order), O (extra OS processing required), G (group), T (TLS),\n");
+    printf("  C (compressed), x (unknown), o (OS specific), E (exclude),\n");
+    printf("  p (processor specific)\n");
 }
 
 /**
@@ -34,20 +43,85 @@ void print_section_headers_32(const Elf32_Shdr *shdr, const char *shstr, int num
  */
 void print_section_headers_64(const Elf64_Shdr *shdr, const char *shstr, int num)
 {
-	int i;
+    int i;
 
-	printf("There are %d section headers, starting at offset 0x%lx:\n\n",
-		num, shdr->sh_offset);
+    printf("There are %d section headers, starting at offset 0x%lx:\n\n",
+           num, shdr->sh_offset);
 
-	printf("Section Headers:\n");
-	printf("  [Nr] Name              Type            Address          Off    Size   ES Flg Lk Inf Al\n");
+    printf("Section Headers:\n");
+    printf("  [Nr] Name              Type            Address          Off    Size   ES Flg Lk Inf Al\n");
 
-	for (i = 0; i < num; i++)
-	{
-		printf("  [%2d] %-17s %-15s %016lx %06lx %06lx %02lx %3s %2d %3u %2lu\n",
-			i, shstr + shdr[i].sh_name, "Section Type",
-			shdr[i].sh_addr, shdr[i].sh_offset, shdr[i].sh_size,
-			shdr[i].sh_entsize, "Flg", shdr[i].sh_link,
-			shdr[i].sh_info, shdr[i].sh_addralign);
-	}
+    for (i = 0; i < num; i++)
+    {
+        printf("  [%2d] %-17s %-15s %016lx %06lx %06lx %02lx %3s %2d %3u %2lu\n",
+               i, shstr + shdr[i].sh_name, get_section_type(shdr[i].sh_type),
+               shdr[i].sh_addr, shdr[i].sh_offset, shdr[i].sh_size,
+               shdr[i].sh_entsize, get_section_flags(shdr[i].sh_flags),
+               shdr[i].sh_link, shdr[i].sh_info, shdr[i].sh_addralign);
+    }
+
+    printf("Key to Flags:\n");
+    printf("  W (write), A (alloc), X (execute), M (merge), S (strings), I (info),\n");
+    printf("  L (link order), O (extra OS processing required), G (group), T (TLS),\n");
+    printf("  C (compressed), x (unknown), o (OS specific), E (exclude),\n");
+    printf("  p (processor specific)\n");
+}
+
+/**
+ * get_section_type - returns a string representation of the section type
+ * @sh_type: section type value
+ * Return: string representation of the section type
+ */
+static const char *get_section_type(uint32_t sh_type)
+{
+    switch (sh_type)
+    {
+    case SHT_NULL:       return "NULL";
+    case SHT_PROGBITS:   return "PROGBITS";
+    case SHT_SYMTAB:     return "SYMTAB";
+    case SHT_STRTAB:     return "STRTAB";
+    case SHT_RELA:       return "RELA";
+    case SHT_HASH:       return "HASH";
+    case SHT_DYNAMIC:    return "DYNAMIC";
+    case SHT_NOTE:       return "NOTE";
+    case SHT_NOBITS:     return "NOBITS";
+    case SHT_REL:        return "REL";
+    case SHT_SHLIB:      return "SHLIB";
+    case SHT_DYNSYM:     return "DYNSYM";
+    case SHT_INIT_ARRAY: return "INIT_ARRAY";
+    case SHT_FINI_ARRAY: return "FINI_ARRAY";
+    case SHT_PREINIT_ARRAY: return "PREINIT_ARRAY";
+    case SHT_GROUP:      return "GROUP";
+    case SHT_SYMTAB_SHNDX: return "SYMTAB_SHNDX";
+    default:             return "UNKNOWN";
+    }
+}
+
+/**
+ * get_section_flags - returns a string representation of the section flags
+ * @sh_flags: section flags value
+ * Return: string representation of the section flags
+ */
+static const char *get_section_flags(uint64_t sh_flags)
+{
+    static char flags[10];
+    int pos = 0;
+
+    if (sh_flags & SHF_WRITE)      flags[pos++] = 'W';
+    if (sh_flags & SHF_ALLOC)      flags[pos++] = 'A';
+    if (sh_flags & SHF_EXECINSTR)  flags[pos++] = 'X';
+    if (sh_flags & SHF_MERGE)      flags[pos++] = 'M';
+    if (sh_flags & SHF_STRINGS)    flags[pos++] = 'S';
+    if (sh_flags & SHF_INFO_LINK)  flags[pos++] = 'I';
+    if (sh_flags & SHF_LINK_ORDER) flags[pos++] = 'L';
+    if (sh_flags & SHF_OS_NONCONFORMING) flags[pos++] = 'O';
+    if (sh_flags & SHF_GROUP)      flags[pos++] = 'G';
+    if (sh_flags & SHF_TLS)        flags[pos++] = 'T';
+    if (sh_flags & SHF_COMPRESSED) flags[pos++] = 'C';
+    if (sh_flags & SHF_MASKOS)     flags[pos++] = 'o';
+    if (sh_flags & SHF_EXCLUDE)    flags[pos++] = 'E';
+    if (sh_flags & SHF_MASKPROC)   flags[pos++] = 'p';
+    flags[pos] = '\0';
+
+    return flags;
 }
