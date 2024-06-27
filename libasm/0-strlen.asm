51eb7888a3e30ba346ddf0866587f7e94179852d
NolbsAtHolbs <you@example.com>
2024-06-27 04:09:52 -0500

    comparison loop

--- libasm/0-strlen.asm
+++ libasm/0-strlen.asm
@@ -1,1 +1,18 @@
+BITS 64
 
+section .data
+
+section .bss
+
+section .text
+global asm_strlen
+
+asm_strlen:
+    xor rax, rax				; Exclusive OR with itself always gives zero
+_byte_compare_loop:
+    cmp byte [rdi + rax], 0		; Compare each byte in str with 0
+    je _strlen_return			; If comparison = 0 (end of str), jump to return statement
+    inc rax						; Otherwise, increment rax
+    jmp _byte_compare_loop 		; Repeat the loop
+_strlen_return:
+    ret							; Return to caller by popping return address with totaled str len in rax
