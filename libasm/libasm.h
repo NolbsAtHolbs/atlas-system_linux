bfbe3df083a4677f014691f11e090770a1ed634a
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2024-06-27 02:35:16 -0500

    Create libasm.h

---
+++ libasm/libasm.h
@@ -0,0 +1,12 @@
+#ifndef LIBASM_H
+#define LIBASM_H
+
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+size_t asm_strlen(const char *str);
+
+#endif
