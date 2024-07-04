095cb58a2937809536aad0c59f1395d273a083b8
NolbsAtHolbs <you@example.com>
2024-07-04 11:40:13 -0500

    syswrite

---
+++ libasm/6-putc.asm
@@ -0,0 +1,22 @@
+BITS 64
+global asm_putc
+section .text
+
+asm_putc:
+    ; Save char to be printed on stack
+    push rdi
+
+    ; Prepare args for write syscall
+    mov rax, 1          ; syscall num for write
+    mov rdi, 1          ; file descriptor 1 (stdout)
+    mov rsi, rsp        ; pointer to the character on the stack
+    mov rdx, 1          ; num of bytes to write
+
+    syscall
+
+    ; Restore stack
+    add rsp, 8
+
+    ; Return num of bytes written
+    mov rax, 1
+    ret
