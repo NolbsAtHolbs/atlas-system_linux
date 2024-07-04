067b6b2b1d9fe4e49d60aa20cdcbaac4ddb5f3b2
NolbsAtHolbs <you@example.com>
2024-07-04 18:05:05 -0500

    test 2

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -5,39 +5,43 @@ section .text
 asm_strncmp:
 	push rbp
 	mov rbp, rsp
+
     ; Initialize pointers and counter
     mov rcx, rdx        ; rcx used as n counter
     test rcx, rcx       ; Is n = 0?
     jz _equal           ; If yes, strings are considered equal
 
-_compare_loop:
+_loop:
     mov al, byte [rdi]  ; Load byte from s1
     mov bl, byte [rsi]  ; Load byte from s2
 
     cmp al, bl			; Compare bytes
-    jne _not_equal      ; If unequal, return difference
+	jg _greater         ; If s1 byte is greater, return 1
+    jl _less            ; If s1 byte is less, return -1
 
-    ; Reached end of either string?
+    ; Check if we reached the end of either string
     test al, al
     jz _equal           ; If end of s1, strings are equal
     test bl, al
     jz _equal           ; If end of s2, strings are equal
 
-    ; Move to next char
+    ; Move to the next character
     inc rdi
     inc rsi
-    dec rcx             ; Decrement counter
-    jnz _compare_loop   ; Until 0
+    dec rcx             ; Decrement the counter
+    jnz _loop			; If counter is not zero, continue loop
 
 _equal:
     xor rax, rax        ; Return 0
     jmp _return
 
-_not_equal:
-    sub al, bl			; Calculate difference between chars
-    movsx rax, al       ; Sign extend result to 64 bits
+_greater:
+    mov rax, 1          ; Return 1
+    jmp _return
 
+_less:
+    mov rax, -1         ; Return -1
 _return:
-	mov rsp, rbp
-	pop rbp
-	ret
+    mov rsp, rbp
+    pop rbp
+    ret
