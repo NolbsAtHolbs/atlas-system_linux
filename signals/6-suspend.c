4bf1977fa2b0a05634cbfa5fa378c80a0a93fbac
NolbsAtHolbs <you@example.com>
2024-07-10 22:06:30 -0500

    1 handle and exit

---
+++ signals/6-suspend.c
@@ -0,0 +1,34 @@
+#include "signals.h"
+
+/**
+ * sigint_tel - Handles SIGINT catch
+ * @sn: Signal number
+ */
+void sigint_tel(int sn)
+{
+	printf("Caught %d\n", sn);
+	printf("Signal received\n");
+	_exit(EXIT_SUCCESS);
+}
+
+/**
+ * main - Sets a handler for SIGINT and suspends the program
+ * Return: EXIT_SUCCESS on successful execution
+ */
+int main(void)
+{
+	struct sigaction sav;
+
+	sav.sa_handler = sigint_tel;
+	sigemptyset(&sav.sa_mask);
+	sav.sa_flags = 0;
+
+	if (sigaction(SIGINT, &sav, NULL) == -1)
+	{
+		perror("sigaction");
+		return (EXIT_FAILURE);
+	}
+	pause();
+
+	return (EXIT_SUCCESS);
+}
