f7352ea4150df7e5efa1f58c5f4a1252e92bccde
NolbsAtHolbs <you@example.com>
2024-07-10 20:21:24 -0500

    set handler & retrieve pointer to it

--- signals/1-current_handler_signal.c
+++ signals/1-current_handler_signal.c
@@ -2,9 +2,14 @@
 
 /**
  * current_handler_signal - retrieves the current handler of the signal SIGINT
- * Return: pointer to current handler of SIGINT, or NULL on failure hopefully
+ * Return: pointer to current handler of SIGINT, or NULL on failure
  */
 void (*current_handler_signal(void))(int)
 {
-	return (signal(SIGINT, SIG_IGN));
+	void (*current_handler)(int) = signal(SIGINT, SIG_IGN);
+	if (signal(SIGINT, current_handler) == SIG_ERR)
+	{
+		return (NULL);
+	}
+	return (current_handler);
 }
