092b617c3f4a54d6ee2bbf8f50e89d09784df34a
NolbsAtHolbs <you@example.com>
2024-07-10 19:55:55 -0500

    we have prototypes again

--- signals/0-handle_signal.c
+++ signals/0-handle_signal.c
@@ -15,7 +15,7 @@ void signal_interrupt_handler(int portablenumber)
  *
  * Return: 0 on success, -1 on error
  */
-int handle_signal_error(void)
+int handle_signal(void)
 {
 	if (signal(SIGINT, signal_interrupt_handler) == SIG_ERR)
 	{
