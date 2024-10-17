d0a90f8d1cc3cdbabc5be293c07ff91a8eefa2a8
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-10-16 23:26:28 -0500

    format test c -> .h

--- sockets/4-request.c
+++ sockets/4-request.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#include "sockets.h"
 
 #define HTTP200 "HTTP/1.1 200 OK\r\n\r\n"
 
