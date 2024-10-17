d9107a1d370d2dec4877e7d27adb4c66b6868011
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-10-17 00:10:00 -0500

    how

--- sockets/6-headers.c
+++ sockets/6-headers.c
@@ -2,7 +2,7 @@
 
 #define HTTP200 "HTTP/1.1 200 OK\r\n\r\n"
 
-void parse_query(char *query);
+void parse_headers(char *query);
 
 /**
  * main - opens an IPv4/TCP socket, and listens to traffic on port 8080
@@ -10,58 +10,108 @@ void parse_query(char *query);
 */
 int main(void)
 {
-	int sock_fd, client_fd;
-	size_t bytes = 0;
-	char buffer[4096], sent[32] = HTTP200;
-	struct sockaddr_in s_address;
-	socklen_t addrlen = sizeof(s_address);
+	int server_fd, client_fd;
+	ssize_t bytes_read;
+	char buffer[4096];
+	struct sockaddr_in server_addr, client_addr;
+	socklen_t addrlen = sizeof(client_addr);
 
-	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock_fd == -1)
-		perror("socket failed"), exit(EXIT_FAILURE);
-	s_address.sin_family = AF_INET, s_address.sin_port = htons(8080);
-	s_address.sin_addr.s_addr = INADDR_ANY;
-	if (bind(sock_fd, (struct sockaddr *)&s_address, sizeof(s_address)) < 0)
-		perror("bind failed"), exit(EXIT_FAILURE);
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+	int opt = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+	{
+		perror("setsockopt failed");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(8080);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	{
+		perror("bind failed");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
 	printf("Server listening on port 8080\n");
-	if (listen(sock_fd, 5) < 0)
-		perror("listen failed"), exit(EXIT_FAILURE);
+	if (listen(server_fd, 5) < 0)
+	{
+		perror("listen failed");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
 	while (1)
 	{
-		client_fd = accept(sock_fd, (struct sockaddr *)&s_address, &addrlen);
+		client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addrlen);
 		if (client_fd < 0)
-			perror("accept failed"), exit(EXIT_FAILURE);
-		printf("Client connected: %s\n", inet_ntoa(s_address.sin_addr));
-		bytes = recv(client_fd, buffer, 4096, 0);
-		if (bytes > 0)
 		{
-			printf("Raw request: \"%s\"\n", buffer), fflush(stdout);
-			parse_query(buffer);
+			perror("accept failed");
+			continue;
 		}
-		send(client_fd, sent, sizeof(sent), 0);
+		printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
+		bytes_read = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+		if (bytes_read > 0)
+		{
+			buffer[bytes_read] = '\0'; /* null-terminate the buffer */
+			printf("Raw request: \"%s\"\n", buffer);
+			parse_headers(buffer);
+		}
+		else if (bytes_read == 0)
+		{
+			printf("Client disconnected\n");
+		}
+		else
+		{
+			perror("recv failed");
+		}
+		if (send(client_fd, HTTP200, strlen(HTTP200), 0) < 0)
+		{
+			perror("send failed");
+		}
 		close(client_fd);
 	}
-	return (0);
+	close(server_fd);
+	return (EXIT_SUCCESS);
 }
 
 /**
- * parse_query - parses supplied http query string
- * @query: supplied query string to parse
-*/
-void parse_query(char *query)
+ * parse_headers - Parses and prints HTTP headers from the buffer
+ * @buffer: The buffer containing the HTTP request
+ */
+void parse_headers(char *buffer)
 {
-	int i = 0, flag = 0;
-	char *token = NULL, *headers[16] = {0}, key[50], val[50];
+	char *line, *headers, *key, *value;
 
-	do {
-		token = strsep(&query, "\r\n");
-		if (token)
-			headers[i++] = token, flag = 1;
-	} while (token && flag--);
+	headers = strstr(buffer, "\r\n");
+	if (!headers)
+		return;
 
-	for (i = 1; headers[i]; i++)
+	headers += 2; /* move past the first CRLF */
+
+	/* iterate over each header line */
+	line = strtok(headers, "\r\n");
+	while (line && line[0] != '\0')
 	{
-		if (sscanf(headers[i],"%[^:]:%s", key, val) > 0)
-			printf("Header: \"%s\" -> \"%s\"\n", key, val), fflush(stdout);
+		key = line;
+		value = strchr(line, ':');
+		if (value)
+		{
+			*value = '\0'; /* null-terminate the key */
+			value++;       /* move past ':' */
+
+			/* skip leading whitespace in value */
+			while (*value == ' ' || *value == '\t')
+				value++;
+
+			printf("Header: \"%s\" -> \"%s\"\n", key, value);
+		}
+		line = strtok(NULL, "\r\n");
 	}
 }
