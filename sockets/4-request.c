ad17c7a31d65b3c91cde0ae9590f5af5354fa7bc
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-10-16 23:15:48 -0500

    format test

--- sockets/4-request.c
+++ sockets/4-request.c
@@ -1,53 +1,45 @@
 #include "sockets.h"
 
+#define MESSAGE "HTTP/1.1 200 OK\r\n\r\n"
+
 /**
  * main - opens an IPv4/TCP socket, listens to traffic on port 8080 @ any addr
  * Return: 0 upon success
  */
 int main(void)
 {
-	int serv_fd, client_fd;
-	struct sockaddr_in serv_addr, client_addr;
-	socklen_t cli_len = sizeof(client_addr);
-	ssize_t bytes_read;
-	char *client_ip, method[8], path[1024], version[16], buffer[BUFSIZ];
+	int sock_fd, cli_fd;
+	size_t bytes = 0;
+	char buffer[4096], meth[50], path[50], ver[50], sent[32] = MESSAGE;
+	struct sockaddr_in s_address;
+	socklen_t addrlen = sizeof(s_address);
 
-	serv_fd = socket(AF_INET, SOCK_STREAM, 0); /* create socket */
-	if (serv_fd == -1)
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock_fd == -1)
 		perror("socket failed"), exit(EXIT_FAILURE);
-	int opt = 1; /* set socket options */
-	if (setsockopt(serv_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
-		perror("setsockopt failed"), exit(EXIT_FAILURE);
-	memset(&serv_addr, 0, sizeof(serv_addr)); /* init serv adr structure */
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(8080);
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	if (bind(serv_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	s_address.sin_family = AF_INET, s_address.sin_port = htons(8080);
+	s_address.sin_addr.s_addr = INADDR_ANY;
+	if (bind(sock_fd, (struct sockaddr *)&s_address, sizeof(s_address)) < 0)
 		perror("bind failed"), exit(EXIT_FAILURE);
 	printf("Server listening on port 8080\n");
-	if (listen(serv_fd, 5) < 0) /* start listening */
+	if (listen(sock_fd, 5) < 0)
 		perror("listen failed"), exit(EXIT_FAILURE);
 	while (1)
-	{ /* accept an incoming connection */
-		client_fd = accept(serv_fd, (struct sockaddr *)&client_addr, &cli_len);
-		if (client_fd < 0)
+	{
+		cli_fd = accept(sock_fd, (struct sockaddr *)&s_address, &addrlen);
+		if (cli_fd < 0)
 			perror("accept failed"), exit(EXIT_FAILURE);
-		client_ip = inet_ntoa(client_addr.sin_addr); /* get client's IP addr */
-		printf("Client connected: %s\n", client_ip);
-		memset(buffer, 0, sizeof(buffer)); /* receive HTTP request */
-		bytes_read = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-		if (bytes_read < 0)
-			perror("recv failed"), exit(EXIT_FAILURE);
-		printf("Raw request: \"%s\"\n", buffer); /* print raw HTTP request */
-		sscanf(buffer, "%7s %1023s %15s", method, path, version); /* parse */
-		printf("Method: %s\n", method);
-		printf("Path: %s\n", path);
-		printf("Version: %s\n", version);
-		const char *response = "HTTP/1.1 200 OK\r\n\r\n"; /* send HTTP200 OK */
-		if (send(client_fd, response, strlen(response), 0) < 0)
-			perror("send failed"), exit(EXIT_FAILURE);
-		close(client_fd);
+		printf("Client cli_fded: %s\n", inet_ntoa(s_address.sin_addr));
+		bytes = recv(cli_fd, buffer, 4096, 0);
+		if (bytes > 0)
+		{
+			printf("Raw request: \"%s\"\n", buffer), fflush(stdout);
+			sscanf(buffer, "%s %s %s", meth, path, ver);
+			printf("Method: %s\nPath: %s\nVersion: %s\n", meth, path, ver);
+			fflush(stdout);
+		}
+		send(cli_fd, sent, sizeof(sent), 0);
+		close(cli_fd);
 	}
-	close(serv_fd);
-	return (EXIT_SUCCESS);
+	return (0);
 }
