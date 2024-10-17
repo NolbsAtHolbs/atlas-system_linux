01968df8ff30c382f2ecc6fc86f8d75ba386fa45
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-10-16 23:33:14 -0500

    revisit

--- sockets/4-request.c
+++ sockets/4-request.c
@@ -8,71 +8,38 @@
  */
 int main(void)
 {
-	int sock_fd, client_fd;
+	int sock_fd, cli_fd;
 	size_t bytes = 0;
-	char buffer[4096], meth[50], path[50], ver[50];
-	const char *response = HTTP200;
-	struct sockaddr_in serv_addr, client_addr;
-	socklen_t addrlen = sizeof(client_addr);
+	char buffer[4096], meth[50], path[50], ver[50], sent[32] = HTTP200;
+	struct sockaddr_in s_address;
+	socklen_t addrlen = sizeof(s_address);
 
 	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock_fd == -1)
-	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(8080);
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-
-	if (bind(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-	{
-		perror("bind failed");
-		close(sock_fd);
-		exit(EXIT_FAILURE);
-	}
+		perror("socket failed"), exit(EXIT_FAILURE);
+	s_address.sin_family = AF_INET, s_address.sin_port = htons(8080);
+	s_address.sin_addr.s_addr = INADDR_ANY;
+	if (bind(sock_fd, (struct sockaddr *)&s_address, sizeof(s_address)) < 0)
+		perror("bind failed"), exit(EXIT_FAILURE);
 	printf("Server listening on port 8080\n");
-
 	if (listen(sock_fd, 5) < 0)
-	{
-		perror("listen failed");
-		close(sock_fd);
-		exit(EXIT_FAILURE);
-	}
+		perror("listen failed"), exit(EXIT_FAILURE);
 	while (1)
 	{
-		client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &addrlen);
-		if (client_fd < 0)
-		{
-			perror("accept failed");
-			close(sock_fd);
-			exit(EXIT_FAILURE);
-		}
-		printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
-		memset(buffer, 0, sizeof(buffer));
-		bytes = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+		cli_fd = accept(sock_fd, (struct sockaddr *)&s_address, &addrlen);
+		if (cli_fd < 0)
+			perror("accept failed"), exit(EXIT_FAILURE);
+		printf("Client cli_fded: %s\n", inet_ntoa(s_address.sin_addr));
+		bytes = recv(cli_fd, buffer, 4096, 0);
 		if (bytes > 0)
 		{
-			buffer[bytes] = '\0';
-			printf("Raw request: \"%s\"\n", buffer);
-			sscanf(buffer, "%49s %49s %49s", meth, path, ver);
-			printf("Method: %s\n", meth);
-			printf("Path: %s\n", path);
-			printf("Version: %s\n", ver);
+			printf("Raw request: \"%s\"\n", buffer), fflush(stdout);
+			sscanf(buffer, "%s %s %s", meth, path, ver);
+			printf("Method: %s\nPath: %s\nVersion: %s\n", meth, path, ver);
+			fflush(stdout);
 		}
-		else if (bytes == 0)
-			printf("Client disconnected unexpectedly\n");
-		else
-		{
-			perror("recv failed");
-			close(client_fd);
-			continue;
-		}
-		if (send(client_fd, response, strlen(response), 0) < 0)
-			perror("send failed");
-		close(client_fd);
+		send(cli_fd, sent, sizeof(sent), 0);
+		close(cli_fd);
 	}
-	close(sock_fd);
 	return (EXIT_SUCCESS);
 }
